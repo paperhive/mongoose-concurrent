9e3045bca2d2d091c290e6d1f3937440b964311b
André Gaul <andre@gaul.io>
2015-05-23 17:29:58 +0200

    add basic functionality of concurrentUpdate()

--- src/index.js
+++ src/index.js
@@ -1,7 +1,56 @@
 'use strict';
 
-function concurrentPlugin(schema, options) {
-  schema.add({_rev: String});
+var _ = require('lodash');
+
+function concurrentPlugin(schema, pluginOptions) {
+  // default plugin options
+  pluginOptions = _.assign({
+    fieldName: '_revision'
+  }, pluginOptions);
+
+  var schemaObj = {};
+  schemaObj[pluginOptions.fieldName] = {type: Number, default: 0};
+  schema.add(schemaObj);
+  schema.index({_id: 1, _revision: 1});
+
+  schema.methods.concurrentUpdate = function(_revision, options, done) {
+    // handle optional argument 'options'
+    if (done === undefined) {
+      done = options;
+      options = {};
+    }
+
+    // overwrite default options
+    options = _.assign({
+      new: true,
+      overwrite: true,
+      runValidators: true
+    }, options);
+
+    var doc = this;
+    var obj = doc.toObject();
+    obj._revision++;
+
+    doc.constructor.findOneAndUpdate(
+      {_id: doc.id, _revision: _revision},
+      obj,
+      options,
+      function(err, updatedDoc) {
+        if (err) {return done(err);}
+
+        // not found -> document does not exist or revision mismatch
+        if (!updatedDoc) {
+          return done(new Error(
+            'update failed: document does not exist or the provided revision ' +
+            'does not match the revision of the stored document. Maybe the ' +
+            'document was updated by someone else after you fetched it?'
+          ));
+        }
+
+        done(null, updatedDoc);
+      }
+    );
+  };
 }
 
 module.exports = concurrentPlugin;
