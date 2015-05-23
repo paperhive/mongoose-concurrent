74597852b87122a39d773fbebff1d22aca0fa4df
André Gaul <andre@gaul.io>
2015-05-23 17:30:20 +0200

    [test] add basic test for concurrentUpdate()

--- test/concurrentPlugin.js
+++ test/concurrentPlugin.js
@@ -3,6 +3,7 @@
 var mongoose = require('mongoose');
 var erase = require('mongoose-erase');
 var async = require('async');
+var should = require('should');
 
 var concurrentPlugin = require('../');
 
@@ -12,18 +13,40 @@ describe('concurrentPlugin()', function() {
     mongoose, 'mongodb://localhost/concurrentPluginTests'
   ));
 
-  function registerArticleSchema() {
-    var articleSchema = new mongoose.Schema({
-      title: String
+  function registerUserSchema(options) {
+    var userSchema = new mongoose.Schema({
+      name: String,
+      age: Number
     });
     // register concurrentPlugin
-    articleSchema.plugin(concurrentPlugin);
-    return mongoose.model('Article', articleSchema);
+    userSchema.plugin(concurrentPlugin, options);
+    return mongoose.model('User', userSchema);
   }
 
+  it('should use the provided revision field name', function(done) {
+    var User = registerUserSchema({fieldName: '_rev'});
+    User.create({name: 'Darth'}, function(err, darth) {
+      darth.should.have.property('_rev').which.is.exactly(0);
+      done();
+    });
+  });
+
   it('should allow saving if revision matches', function(done) {
-    var Article = registerArticleSchema();
-    done();
+    var User = registerUserSchema();
+
+    async.waterfall([
+      function(cb) {
+        User.create({name: 'Darth', age: 42}, cb);
+      },
+      function(darth, cb) {
+        darth.name = 'Darth Vader';
+        darth.concurrentUpdate(darth._revision, cb);
+      }
+    ], function(err, darth) {
+      if (err) {return done(err);}
+      darth.should.have.property('_revision').which.is.exactly(1);
+      done();
+    });
   });
 
 });
