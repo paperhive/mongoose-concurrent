f52c2ab64130498ffbdd01fd27c142faef14988a
André Gaul <andre@gaul.io>
2015-05-24 16:20:43 +0200

    add concurrentRemove()

--- src/index.js
+++ src/index.js
@@ -8,10 +8,15 @@ function concurrentPlugin(schema, pluginOptions) {
     fieldName: '_revision'
   }, pluginOptions);
 
+  // add revision field
   var schemaObj = {};
   schemaObj[pluginOptions.fieldName] = {type: Number, default: 0};
   schema.add(schemaObj);
-  schema.index({_id: 1, _revision: 1});
+
+  // add index on _id and revision field
+  var schemaIndex = {_id: 1};
+  schemaIndex[pluginOptions.fieldName] = 1;
+  schema.index(schemaIndex);
 
   schema.methods.concurrentUpdate = function(_revision, options, done) {
     // handle optional argument 'options'
@@ -48,6 +53,38 @@ function concurrentPlugin(schema, pluginOptions) {
         }
 
         done(null, updatedDoc);
+      }
+    );
+  };
+
+  schema.methods.concurrentRemove = function(_revision, options, done) {
+    // handle optional argument 'options'
+    if (done === undefined) {
+      done = options;
+      options = {};
+    }
+
+    // overwrite default options
+    options = _.assign({}, options);
+
+    var doc = this;
+
+    doc.constructor.findOneAndRemove(
+      {_id: doc.id, _revision: _revision},
+      options,
+      function(err, removedDoc) {
+        if (err) {return done(err);}
+
+        // not found -> document does not exist or revision mismatch
+        if (!removedDoc) {
+          return done(new Error(
+            'remove failed: document does not exist or the provided revision ' +
+            'does not match the revision of the stored document. Maybe the ' +
+            'document was updated by someone else after you fetched it?'
+          ));
+        }
+
+        done(null, removedDoc);
       }
     );
   };
